d134bfca44ffb5589fec9cfe18d9426da05bf928
Dave Mizrahi <davem@Daves-MBP.home>
2019-05-23 18:37:24 -0400

    database URI

--- app/database/index.js
+++ app/database/index.js
@@ -6,12 +6,16 @@ var logger 		= require('../logger');
 
 // Connect to the database
 // construct the database URI and encode username and password.
-var dbURI = "mongodb://" + 
-			encodeURIComponent(config.db.username) + ":" + 
-			encodeURIComponent(config.db.password) + "@" + 
-			config.db.host + ":" + 
-			config.db.port + "/" + 
-			config.db.name;
+// mongodb+srv://mizmusic:<password>@cluster0-qzb2u.mongodb.net/test?retryWrites=true
+var dbURI = "mongodb+srv://" +
+			encodeURIComponent(config.db.username) + ":" +
+			encodeURIComponent(config.db.password) + "@" +
+			// config.db.host + ":" +
+			config.db.host + "/" +
+			// config.db.port + "/" +
+			config.db.name + "?" +
+			"retryWrites=true"
+
 Mongoose.connect(dbURI, { useNewUrlParser: true });
 
 // Throw an error if the connection fails
@@ -19,12 +23,12 @@ Mongoose.connection.on('error', function(err) {
 	if(err) throw err;
 });
 
-// mpromise (mongoose's default promise library) is deprecated, 
+// mpromise (mongoose's default promise library) is deprecated,
 // Plug-in your own promise library instead.
 // Use native promises
 Mongoose.Promise = global.Promise;
 
-module.exports = { Mongoose, 
+module.exports = { Mongoose,
 	models: {
 		user: require('./schemas/user.js'),
 		room: require('./schemas/room.js')
